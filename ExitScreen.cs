7d9395f6833ee2f7052b356563080de9d48d7dfa
Marcus Coley <marcus.09@live.com>
2014-06-07 13:20:33 -0700

    Exit screen
    The exit screen, uses the screen base class

---
+++ ExitScreen.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace BrickBreaker.Brothers
+{
+    class ExitScreen : Screen
+    {
+        public ExitScreen(Game game, SpriteBatch spriteBatch)
+            : base(game, spriteBatch)
+        {
+           
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+            this.Game.Exit();
+            base.Update(gameTime);
+        }
+    }
+}
