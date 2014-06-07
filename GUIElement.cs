26a922352183b79d82332c85c0e02d78a1ad16c5
Marcus Coley <marcus.09@live.com>
2014-06-07 13:21:02 -0700

    Base class for all GUI elements
    Base class for all GUI elements

---
+++ GUIElement.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace BrickBreaker.Brothers
+{
+    class GUIElement
+    {
+        public GUIElement(Game game)
+        {
+        }
+
+        protected Rectangle boundingBox;
+        public Rectangle BoundingBox
+        {
+            get { return boundingBox; }
+        }
+
+        public virtual void CheckBounds(Rectangle reference, Screen sender = null)
+        {
+        }
+
+        public virtual void Draw(SpriteBatch spriteBatch)
+        {
+        }
+    }
+}
