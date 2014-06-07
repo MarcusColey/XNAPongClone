14489653440a6bd44defdc2e0fbc694362320cd6
Marcus Coley <marcus.09@live.com>
2014-06-07 13:25:09 -0700

    Brick type
    The hardest brick to destroy

---
+++ MegaBrick.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework;
+
+namespace BrickBreaker.Brothers
+{
+    class MegaBrick : Brick
+    {
+        public MegaBrick(Texture2D sprite, Vector2 position, Rectangle boundingBox)
+            : base(sprite, position, boundingBox)
+        {
+            breakHitCount = 3;
+            hitCount = 0;
+            pointWorth = 25;
+        }
+
+        public override int Hit()
+        {
+            return base.Hit();
+        }
+
+        public override void Draw(SpriteBatch spritebatch)
+        {
+            base.Draw(spritebatch);
+        }
+    }
+}
