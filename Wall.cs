c12d42600aca3be760f965cac7fdb178e7ed0464
Marcus Coley <marcus.09@live.com>
2014-06-07 13:27:23 -0700

    Handles the functionality for the barriers
    Code the the barriers on the top/left/right sides of the screen

---
+++ Wall.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework;
+
+namespace BrickBreaker.Brothers
+{
+    class Wall : GameObject
+    {
+        bool isVertical;
+        public bool IsVertical
+        {
+            get
+            {
+                return isVertical;
+            }
+        }
+
+        public Wall(Texture2D sprite, Vector2 position, Rectangle boundingBox, bool setVertical = false)
+            : base(sprite, position, boundingBox)
+        {
+            isVertical = setVertical;
+        }
+
+        public override void Draw(SpriteBatch spritebatch)
+        {
+            base.Draw(spritebatch);
+        }
+    }
+}
