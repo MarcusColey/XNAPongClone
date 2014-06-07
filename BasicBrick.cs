9e469bb6b9341023e2410bb377c708363f6e2f09
Marcus Coley <marcus.09@live.com>
2014-06-07 13:17:22 -0700

    A brick type
    The easiest brick type to destroy, inherits from the Brick class

---
+++ BasicBrick.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework;
+
+namespace BrickBreaker.Brothers
+{
+    public class BasicBrick : Brick
+    {
+        public BasicBrick(Texture2D sprite, Vector2 position, Rectangle rect)
+            :base(sprite, position, rect)
+        {
+            breakHitCount = 1;
+            hitCount = 0;
+            pointWorth = 5;
+
+            Console.WriteLine("Brick Width: " + sprite.Width);
+            Console.WriteLine("Brick Height: " + sprite.Height);
+        }
+
+        public override int Hit()
+        {
+            return base.Hit();
+        }
+
+        public override void Draw(SpriteBatch spritebatch)
+        {
+            base.Draw(spritebatch);
+        }
+    }
+}
