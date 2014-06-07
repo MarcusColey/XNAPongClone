045e215f944636d05bc9e5c251fbc227e7a9580d
Marcus Coley <marcus.09@live.com>
2014-06-07 13:25:45 -0700

    Base class for all screens
    Base class for all screens

---
+++ Screen.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace BrickBreaker.Brothers
+{
+    class Screen : DrawableGameComponent
+    {
+        public delegate void ChangeScreen(int screenNumber);
+        public event ChangeScreen OnChangeScreen;
+
+        public Screen(Game game, SpriteBatch spritBatch)
+            : base(game)
+        {
+            
+        }
+
+        public virtual void ChangeScreens(int screenNumber)
+        {
+        }
+
+        protected void InvokeOnChangeScreen(int screenNumber)
+        {
+            OnChangeScreen(screenNumber);
+        }
+    }
+}
