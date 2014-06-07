6a57080c8bebd43620bd63f16401694baa936a5a
Marcus Coley <marcus.09@live.com>
2014-06-07 13:17:46 -0700

    Base class for all bricks
    Acts as the base class for all brick types

---
+++ Brick.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework;
+
+namespace BrickBreaker.Brothers
+{
+    public class Brick : GameObject
+    {
+        protected int breakHitCount, hitCount, pointWorth;
+
+        public Brick(Texture2D sprite, Vector2 position, Rectangle rect)
+            : base(sprite, position, rect)
+        {
+        }
+
+        public virtual int GetPoints()
+        {
+            return pointWorth;
+        }
+
+        public virtual int Hit()
+        {
+            hitCount++;
+            return breakHitCount - hitCount;
+        }
+    }
+}
