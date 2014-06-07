1b1a30434282bda3fcac0c60480b6a49c3ad4ecd
Marcus Coley <marcus.09@live.com>
2014-06-07 13:25:59 -0700

    Manages all screens for display
    Manages all screens for display

---
+++ ScreenManager.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using System.Threading;
+
+namespace BrickBreaker.Brothers
+{
+    class ScreenManager : GameComponent
+    {
+        List<Screen> screens;
+        int current;
+
+        public ScreenManager(Game game)
+            : base(game)
+        {
+            screens = new List<Screen>();
+        }
+
+        public void AddScreen(Screen screen, Game game)
+        {
+            screen.Initialize();
+            screens.Add(screen);
+        }
+
+        public void Initialize(int startingScreen)
+        {
+            current = startingScreen;
+            Game.Components.Add(screens[startingScreen]);
+        }
+
+        public void ChangeScreens(int screenNumber)
+        {
+            // Forget the previous screen
+            Game.Components.Remove(screens[current]);
+
+            // Display the desired screen
+            current = screenNumber;
+            Game.Components.Add(screens[current]);
+        }
+    }
+}
